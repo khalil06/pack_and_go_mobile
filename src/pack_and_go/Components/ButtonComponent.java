cf934b25830f2dd58ff35330b2ea2106608e4ae2
khalil <khalil.chettaoui06@gmail.com>
2022-05-11 20:11:57 +0100

    last commit material theme

--- src/pack_and_go/Components/ButtonComponent.java
+++ src/pack_and_go/Components/ButtonComponent.java
@@ -7,14 +7,14 @@ import com.codename1.ui.plaf.Style;
 public class ButtonComponent {
     public com.codename1.ui.Button getButton(String buttonText){
         com.codename1.ui.Button btn = new com.codename1.ui.Button(buttonText);
-        Style btnStyle= new Style();
+/*        Style btnStyle= new Style();
         btnStyle.setBgColor(ColorUtil.rgb(68, 184, 149));
-        //next.getUnselectedStyle().setBgColor(ColorUtil.rgb(104, 222, 218),true);
+        btn.getUnselectedStyle().setBgColor(ColorUtil.rgb(104, 222, 218),true);
         btn.setUnselectedStyle(btnStyle);
         btn.getUnselectedStyle().setAlignment(Component.CENTER);
         btn.getUnselectedStyle().setMargin(100, 0, 200, 200);
         btn.getUnselectedStyle().setPadding(5, 5, 1, 1);
-        btn.getUnselectedStyle().setFgColor(ColorUtil.WHITE);
+        btn.getUnselectedStyle().setFgColor(ColorUtil.WHITE);*/
         return btn;
     }
 }
