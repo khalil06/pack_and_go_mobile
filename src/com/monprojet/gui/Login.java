c2f1ab1cbeeb64e145037b7867cc5088897c4303
Amir Daghsen <amir.daghsen@esprit.tn>
2022-05-11 21:35:07 +0100

    $

---
+++ src/com/monprojet/gui/Login.java
@@ -0,0 +1,31 @@
+package com.monprojet.gui;
+
+import com.monprojet.MainApp;
+import com.codename1.ui.*;
+import com.codename1.ui.layouts.*;
+
+public class Login extends Form {
+
+    public static Form loginForm;
+
+    public Login() {
+        super("Connexion", new BoxLayout(BoxLayout.Y_AXIS));
+        loginForm = this;
+        addGUIs();
+    }
+
+    private void addGUIs() {
+
+        
+        Button frontendBtn = new Button("Front");
+        frontendBtn.addActionListener(l -> new com.monprojet.gui.front.AccueilFront().show());
+        this.add(frontendBtn);
+        
+        
+        Button backendBtn = new Button("Back");
+        backendBtn.addActionListener(l -> new com.monprojet.gui.back.AccueilBack().show());
+
+        this.add(backendBtn);
+    }
+    
+}
---
+++ src/com/monprojet/utils/Statics.java
@@ -0,0 +1,11 @@
+package com.monprojet.utils;
+
+public class Statics {
+    public static final String BASE_URL = "http://127.0.0.1:8000/mobile";
+    
+    public static final String COMPANY_IMAGE_URL = "http://127.0.0.1:8000/mobile/company/image/";
+    
+    
+    
+    public static String compareVar;
+}
